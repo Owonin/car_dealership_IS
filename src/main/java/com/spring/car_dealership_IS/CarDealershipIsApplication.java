8225fed3a765cbc0e31ab22ede88abece4daa45e
Owonin <RinGrayRat@yandex.ru>
2021-06-14 14:43:42 +0300

    Initial commit

---
+++ src/main/java/com/spring/car_dealership_IS/CarDealershipIsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.car_dealership_IS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/car_dealership_IS/CarDealershipIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.car_dealership_IS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealershipIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
