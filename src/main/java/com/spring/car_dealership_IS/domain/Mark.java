83aadaaeacd92a2c29e7668e9de419ae32e06165
Owonin <RinGrayRat@yandex.ru>
2021-09-30 16:20:15 +0300

    Base model crated

--- src/main/java/com/spring/car_dealership_IS/domain/Mark.java
+++ src/main/java/com/spring/car_dealership_IS/domain/Mark.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Objects;
+
 
 @Document
 @AllArgsConstructor
@@ -13,6 +15,20 @@ public class Mark extends BaseModelWithImg {
 
     public String getCountryName() {
         return CountryName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Mark)) return false;
+        if (!super.equals(o)) return false;
+        Mark mark = (Mark) o;
+        return Objects.equals(CountryName, mark.CountryName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), CountryName);
     }
 
     public void setCountryName(String countryName) {
